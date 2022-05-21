dea1d657436df24e89590a94a287efd9bd8995f9
andrada-strimbu <andrada.strimbu@gmail.com>
2022-05-21 17:55:31 +0300

    laboratorul_4

---
+++ tema_sapt 4/Sort.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include "Sort.h"
+
+using namespace std;
+
+void Sort::InsertSort(bool ascendent)
+{
+
+}
+
+void Sort::QuickSort(bool ascendent)
+{
+}
+
+void Sort::BubbleSort(bool ascendent)
+{
+}
+
+void Sort::Print()
+{
+}
+
+int Sort::GetElementsCount()
+{
+	return 0;
+}
+
+int Sort::GetElementFromIndex(int index)
+{
+	return 0;
+}
---
+++ tema_sapt 4/Sort.h
@@ -0,0 +1,25 @@
+#pragma once
+class Sort
+
+{
+    // add data members
+
+public:
+
+    // add constuctors
+
+    void InsertSort(bool ascendent = false);
+
+    void QuickSort(bool ascendent = false);
+
+    void BubbleSort(bool ascendent = false);
+
+    void Print();
+
+    int  GetElementsCount();
+
+    int  GetElementFromIndex(int index);
+
+}
+
+
